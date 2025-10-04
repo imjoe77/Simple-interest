db1540c95962e8228ff7704daceb9e4e855485a3
Joe <nathanielbandi636@gmail.com>
2025-10-04 09:31:25 +0530

    Update simp.py

--- simp.py
+++ simp.py
@@ -8,5 +8,8 @@ time = float(input("Enter the Time (in years): "))
 # Formula: SI = (P * R * T) / 100
 simple_interest = (principal * rate * time) / 100
 
+#Compound interest
+compound_interest = principal * (1 + rate/100) ** time - principal
+
 # Output
 print("Simple Interest is:", simple_interest)
